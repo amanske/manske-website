b68eb9b68edc60a66e168aa08ee5c03675f05489
amanske <alexanderjanmanske@gmail.com>
2017-03-05 23:25:59 +0100

    Implemented dot navigation

--- js/mainScreen.js
+++ js/mainScreen.js
@@ -1,7 +1,18 @@
-$(document).ready(getHeaderHeight);
-$(window).resize(getHeaderHeight);
+$(document).ready(function () {
+	getHeaderHeight();
+	getFooterHeight();
+});
+$(window).resize(function () {
+	getHeaderHeight();
+	getFooterHeight();
+});
+
 
 function getHeaderHeight () {
 	var height = $('.btn-group').height();
 	$('.headerLeft').css('height', height+'px');
+}
+function getFooterHeight () {
+	var height = $('.footer').outerHeight();
+	$('.dotNavigation').css('margin-bottom', (height + $('dotNavigation').height())+'px');
 }
