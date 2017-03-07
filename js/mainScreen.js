e7a5ba8edef15da396ac4dd4626ab3c32166e21e
amanske <alexanderjanmanske@gmail.com>
2017-03-07 18:58:38 +0100

    Implemented multiple pages

--- js/mainScreen.js
+++ js/mainScreen.js
@@ -1,18 +1,43 @@
 $(document).ready(function () {
-	getHeaderHeight();
-	getFooterHeight();
+	setCorrectHeaderHeight();
+	setCorrectDotMargin();
+
+	$('#homeDot').click(function () {
+		$('wrapper').clearQueue();
+	    $('.wrapper').animate({marginLeft: '0'},1000);
+
+	});
+	$('#aboutDot').click(function () {
+		$('wrapper').clearQueue();
+	    $('.wrapper').animate({marginLeft: '-100%'},1000);
+
+	});
+	$('#resumeDot').click(function () {
+	    $('.wrapper').animate({marginLeft: '-200%'},1000);
+
+	});
+	$('#connectDot').click(function () {
+	    $('.wrapper').animate({marginLeft: '-300%'},1000);
+
+	});
+
 });
 $(window).resize(function () {
-	getHeaderHeight();
-	getFooterHeight();
+	setCorrectHeaderHeight();
+	setCorrectDotMargin();
 });
 
 
-function getHeaderHeight () {
+function setCorrectHeaderHeight () {
 	var height = $('.btn-group').height();
 	$('.headerLeft').css('height', height+'px');
 }
-function getFooterHeight () {
+function setCorrectDotMargin () {
 	var height = $('.footer').outerHeight();
 	$('.dotNavigation').css('margin-bottom', (height + $('dotNavigation').height())+'px');
 }
+function buttonToDot (type) {
+	var dot = document.getElementById(type);
+	dot.click();
+}
+
